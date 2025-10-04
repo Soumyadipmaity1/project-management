980a989827c3151843f591630507fbfb972ea13c
Soumyadip Maity <22053029@kiit.ac.in>
2025-10-04 19:42:58 +0530

    Replace LeadPanel with ProjectLeadPanel in PanelLead component

--- app/project-lead/page.tsx
+++ app/project-lead/page.tsx
@@ -1,9 +1,9 @@
-import LeadPanel from '@/components/Lead/Dashboard/LeadPanel'
+import ProjectLeadPanel from '@/components/ProjectLead/projectLeadPanel'
 
 import React from 'react'
 
 export default function PanelLead()  {
   return (
-    <LeadPanel />
+    <ProjectLeadPanel />
   )
 }
