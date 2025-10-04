9c36816cc49818c9cce54776ca66446fd7fc1f81
Soumyadip Maity <22053029@kiit.ac.in>
2025-10-04 21:54:45 +0530

    Comment out token validation checks in middleware for future implementation

--- middleware.ts
+++ middleware.ts
@@ -7,16 +7,16 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const url = request.nextUrl;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/signin", request.url));
+  // }
 
-  if (
-    token &&
-    (url.pathname.startsWith("/signin") || url.pathname.startsWith("/signup"))
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (
+  //   token &&
+  //   (url.pathname.startsWith("/signin") || url.pathname.startsWith("/signup"))
+  // ) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   // if (url.pathname.startsWith("/admin") && token.role !== "Admin") {
   //   return NextResponse.redirect(new URL("/unauthorized", request.url));
