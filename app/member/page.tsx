afe4ca55561bfe1d8a7b8f7f6c15da0285048b2f
Archi Shaw <archishaw2004@gmail.com>
2025-10-31 23:54:37 +0530

    changes

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -1,11 +1,10 @@
 import MemberPanel from '@/components/Member/Dasboard/MemberPanel'
 import React from 'react'
 
-export default function MPanel()  {
+export default function MPanel() {
   return (
     <div className="w-full min-h-screen text-white">
-        <MemberPanel />
-     
+      <MemberPanel role="Member" /> 
     </div>
   )
 }
